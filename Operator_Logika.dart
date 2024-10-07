f5c5f04e6f84b6fcdc90df6e169923470c3324df
Arifin-only <arifin180104@gmail.com>
2024-10-07 20:07:14 +0800

    Create Operator_Logika.dart

---
+++ Operator_Logika.dart
@@ -0,0 +1,19 @@
+ void main(){
+   var nilaiAkhir = 90;
+   var nilaiTugas = 50;
+
+   var apakahNilaiAkhirBagus = nilaiAkhir >= 75;
+   var apakahNilaiTugasBagus = nilaiTugas >= 75;
+
+   print(apakahNilaiAkhirBagus);
+   print(apakahNilaiTugasBagus);
+
+   var lulus = apakahNilaiAkhirBagus && apakahNilaiTugasBagus;
+   var lulus2 = apakahNilaiAkhirBagus || apakahNilaiTugasBagus;
+
+   print(lulus);
+
+
+   print(!true);
+   print(!false);
+}
