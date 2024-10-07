8713a5c426240988c1a57f56c95dbf8229f99141
Arifin-only <arifin180104@gmail.com>
2024-10-07 19:56:36 +0800

    Create Operator_Aritmatika.dart

---
+++ Operator_Aritmatika.dart
@@ -0,0 +1,17 @@
+void main(){
+   var hasil1 = 10 + 20;
+   var hasil2 = 11 - 3;
+   var hasil3 = -20;
+   var hasil4 = 12 * 3;
+   var hasil5 = 10 / 6;
+   var hasil6 = 10 ~/ 6;
+   var hasil7 = 19 % 5;
+
+   print(hasil1);
+   print(hasil2);
+   print(hasil3);
+   print(hasil4);
+   print(hasil5);
+   print(hasil6);
+   print(hasil7);
+ }
